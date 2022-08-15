fd29435a61479bb2a81aee072c583ed3438b169d
Tatiana Azarchenko <tazarchenko@Tatianas-MacBook-Air.local>
2022-08-15 22:29:12 +0200

    no

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect(process.env.MONGODB_URL, {
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true
+// }) 
+
+mongoose.connect("mongodb+srv://tazarchenko:Gagarin2019@cluster0.r2uon.mongodb.net/album?retryWrites=true&w=majority", {
     useNewUrlParser: true
 }) 
+
