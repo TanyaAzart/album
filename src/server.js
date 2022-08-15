b829e071456eaab439740c8f21df95ccf31587bd
Tatiana Azarchenko <tazarchenko@Tatianas-MacBook-Air.local>
2022-08-15 22:54:42 +0200

    path fixed

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV ==='production') {
     server.use(express.static('client/build'))
 
     server.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+        res.sendFile(path.join(__dirname + '/../client/build/index.html'))
     })
 }
 
