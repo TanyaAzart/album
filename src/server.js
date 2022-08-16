0da47ee3681bfc91f0647924120623a0f9ce2862
Tatiana Azarchenko <tazarchenko@Tatianas-MacBook-Air.local>
2022-08-16 16:19:15 +0200

    fuck

--- src/server.js
+++ src/server.js
@@ -27,10 +27,10 @@ server.use(commentRouter)
 server.use(express.static('client/public'))
 
 if (process.env.NODE_ENV ==='production') {
-    server.use(express.static(path.resolve(__dirname, "./client/build")))
+    server.use(express.static(path.resolve(__dirname, "./../client/build")))
 
     server.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "./../client/build", "index.html"))
     })
 }
 
